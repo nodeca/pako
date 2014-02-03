ff0a4c0c501d57f8f1966ac3eaa4baf8d3d2cbeb
Vitaly Puzrin <vitaly@rcdesign.ru>
2014-02-04 00:54:27 +0400

    updated zlib benchmark to use streams

--- benchmark/implementations/deflate-zlib/index.js
+++ benchmark/implementations/deflate-zlib/index.js
@@ -5,6 +5,35 @@ var zlib = require('zlib');
 exports.async = true;
 
 exports.run = function(data, callback) {
-  var buffer = new Buffer(data);
-  zlib.deflate(buffer, callback);
+  //zlib.deflate(new Buffer(data), callback);
+
+  var zlibStream = zlib.createDeflate({
+    chunkSize: 1*1024*1024/*,
+    level: 6*/
+  });
+  var buffers = [], nread = 0;
+
+
+  zlibStream.on('error', function(err) {
+    zlibStream.removeAllListeners();
+    zlibStream=null;
+    callback(err);
+  });
+
+  zlibStream.on('data', function(chunk) {
+    buffers.push(chunk);
+    nread += chunk.length;
+  });
+
+  zlibStream.on('end', function() {
+    zlibStream.removeAllListeners();
+    zlibStream=null;
+
+    var buffer = Buffer.concat(buffers);
+
+    callback(null);
+  });
+
+  zlibStream.write(new Buffer(data));
+  zlibStream.end();
 }
