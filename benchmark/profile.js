c19d9ce8aa0fba92c4adfd05f9de9b20d8b325ef
Vitaly Puzrin <vitaly@rcdesign.ru>
2014-02-14 09:06:22 +0400

    Added profiling test file

---
+++ benchmark/profile.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var fs   = require('fs');
+var pako = require('../index.js');
+
+var data = new Uint8Array(fs.readFileSync(__dirname +'/samples/lorem_1mb.txt'));
+
+
+for (var i=0; i<50; i++) {
+  pako.deflate(data, { level: 0 });
+}
