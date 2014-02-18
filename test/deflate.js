5dfa74f2e287376d34cebd0304f4aa6eb6ca6a4d
Vitaly Puzrin <vitaly@rcdesign.ru>
2014-02-18 23:15:27 +0400

    Improved test suite

--- test/deflate.js
+++ test/deflate.js
@@ -5,37 +5,33 @@
 
 
 var zlib = require('zlib');
-var fs   = require('fs');
-var path = require('path');
-
 
 var pako    = require('../index');
 var helpers = require('./helpers');
 var testDeflate = helpers.testDeflate;
 
 
-var sample_file = path.join(__dirname, 'fixtures/lorem_100k.txt');
-var sample = new Uint8Array(fs.readFileSync(sample_file));
+var samples = helpers.loadSamples();
 
 
 describe('Deflate defaults', function () {
 
   it('deflate', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, {}, done);
+    testDeflate(zlib.createDeflate, pako.deflate, samples, {}, done);
   });
 
   it('deflate raw', function(done) {
-    testDeflate(zlib.createDeflateRaw, pako.deflateRaw, sample, {}, done);
+    testDeflate(zlib.createDeflateRaw, pako.deflateRaw, samples, {}, done);
   });
 
   it('deflate raw (level 0)', function(done) {
-    testDeflate(zlib.createDeflateRaw, pako.deflateRaw, sample, { level: 0 }, done);
+    testDeflate(zlib.createDeflateRaw, pako.deflateRaw, samples, { level: 0 }, done);
   });
 
   // OS_CODE can differ. Probably should add param to compare function
   // to ignore some buffer positions
   it.skip('gzip', function(done) {
-    testDeflate(zlib.createGzip, pako.gzip, sample, {}, done);
+    testDeflate(zlib.createGzip, pako.gzip, samples, {}, done);
   });
 });
 
@@ -43,64 +39,64 @@ describe('Deflate defaults', function () {
 describe('Deflate levels', function () {
 
   it('level 9', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { level: 9 }, done);
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { level: 9 }, done);
   });
   it('level 8', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { level: 8 }, done);
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { level: 8 }, done);
   });
   it('level 7', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { level: 7 }, done);
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { level: 7 }, done);
   });
   it('level 6', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { level: 6 }, done);
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { level: 6 }, done);
   });
   it('level 5', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { level: 5 }, done);
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { level: 5 }, done);
   });
-  it.skip('level 4', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { level: 4 }, done);
+  it('level 4', function(done) {
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { level: 4 }, done);
   });
   it('level 3', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { level: 3 }, done);
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { level: 3 }, done);
   });
   it('level 2', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { level: 2 }, done);
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { level: 2 }, done);
   });
   it('level 1', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { level: 1 }, done);
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { level: 1 }, done);
   });
-  it('level 0', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { level: 0 }, done);
+  it.skip('level 0', function(done) {
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { level: 0 }, done);
   });
 
 });
 
 
-describe('Deflate windowBits', function () {
+describe.skip('Deflate windowBits', function () {
 
   it('windowBits 15', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { windowBits: 15 }, done);
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { windowBits: 15 }, done);
   });
   it('windowBits 14', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { windowBits: 14 }, done);
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { windowBits: 14 }, done);
   });
-  it.skip('windowBits 13', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { windowBits: 13 }, done);
+  it('windowBits 13', function(done) {
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { windowBits: 13 }, done);
   });
-  it.skip('windowBits 12', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { windowBits: 12 }, done);
+  it('windowBits 12', function(done) {
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { windowBits: 12 }, done);
   });
-  it.skip('windowBits 11', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { windowBits: 11 }, done);
+  it('windowBits 11', function(done) {
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { windowBits: 11 }, done);
   });
-  it.skip('windowBits 10', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { windowBits: 10 }, done);
+  it('windowBits 10', function(done) {
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { windowBits: 10 }, done);
   });
-  it.skip('windowBits 9', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { windowBits: 9 }, done);
+  it('windowBits 9', function(done) {
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { windowBits: 9 }, done);
   });
-  it.skip('windowBits 8', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { windowBits: 8 }, done);
+  it('windowBits 8', function(done) {
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { windowBits: 8 }, done);
   });
 
 });
@@ -109,31 +105,31 @@ describe('Deflate windowBits', function () {
 describe('Deflate memLevel', function () {
 
   it('memLevel 9', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { memLevel: 9 }, done);
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { memLevel: 9 }, done);
   });
   it('memLevel 8', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { memLevel: 8 }, done);
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { memLevel: 8 }, done);
   });
-  it.skip('memLevel 7', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { memLevel: 7 }, done);
+  it('memLevel 7', function(done) {
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { memLevel: 7 }, done);
   });
   it('memLevel 6', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { memLevel: 6 }, done);
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { memLevel: 6 }, done);
   });
   it('memLevel 5', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { memLevel: 5 }, done);
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { memLevel: 5 }, done);
   });
-  it.skip('memLevel 4', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { memLevel: 4 }, done);
+  it('memLevel 4', function(done) {
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { memLevel: 4 }, done);
   });
-  it.skip('memLevel 3', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { memLevel: 3 }, done);
+  it('memLevel 3', function(done) {
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { memLevel: 3 }, done);
   });
-  it.skip('memLevel 2', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { memLevel: 2 }, done);
+  it('memLevel 2', function(done) {
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { memLevel: 2 }, done);
   });
-  it.skip('memLevel 1', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { memLevel: 1 }, done);
+  it('memLevel 1', function(done) {
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { memLevel: 1 }, done);
   });
 
 });
@@ -142,19 +138,19 @@ describe('Deflate memLevel', function () {
 describe.skip('Deflate strategy', function () {
 
   it('Z_DEFAULT_STRATEGY', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { strategy: 0 }, done);
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { strategy: 0 }, done);
   });
   it('Z_FILTERED', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { strategy: 1 }, done);
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { strategy: 1 }, done);
   });
   it('Z_HUFFMAN_ONLY', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { strategy: 2 }, done);
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { strategy: 2 }, done);
   });
   it('Z_RLE', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { strategy: 3 }, done);
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { strategy: 3 }, done);
   });
   it('Z_FIXED', function(done) {
-    testDeflate(zlib.createDeflate, pako.deflate, sample, { strategy: 4 }, done);
+    testDeflate(zlib.createDeflate, pako.deflate, samples, { strategy: 4 }, done);
   });
 
 });
--- test/helpers.js
+++ test/helpers.js
@@ -1,6 +1,33 @@
 'use strict';
 
 
+var fs    = require('fs');
+var path  = require('path');
+var _     = require('lodash');
+var async = require('async');
+
+// Load fixtures to test
+// return: { 'filename1': content1, 'filename2': content2, ...}
+//
+function loadSamples() {
+  var result = {};
+  var dir = path.join(__dirname, 'fixtures');
+
+  fs.readdirSync(dir).sort().forEach(function (sample) {
+    var filepath = path.join(dir, sample),
+        extname  = path.extname(filepath),
+        basename = path.basename(filepath, extname),
+        content  = new Uint8Array(fs.readFileSync(filepath));
+
+    if (basename[0] === '_') { return; } // skip files with name, started with dash
+
+    result[basename] = content;
+  });
+
+  return result;
+}
+
+
 // Compare 2 buffers (can be Array, Uint8Array, Buffer).
 //
 function cmpBuf(a, b) {
@@ -21,7 +48,7 @@ function cmpBuf(a, b) {
 // Helper to test deflate/deflateRaw with different options.
 // Use zlib streams, because it's the only way to define options.
 //
-function testDeflate(zlib_factory, pako_deflate, data, options, callback) {
+function testDeflateSingle(zlib_factory, pako_deflate, data, options, callback) {
 
   var zlibStream = zlib_factory(options);
   var buffers = [], nread = 0;
@@ -59,6 +86,25 @@ function testDeflate(zlib_factory, pako_deflate, data, options, callback) {
   zlibStream.end();
 }
 
+function testDeflate(zlib_factory, pako_deflate, samples, options, callback) {
+  var queue = [];
+
+  _.forEach(samples, function(data, name) {
+    queue.push(function (done) {
+      testDeflateSingle(zlib_factory, pako_deflate, data, options, function (err) {
+        if (err) {
+          done('Error in "' + name + '" - zlib result != pako result');
+          return;
+        }
+        done();
+      });
+    });
+  });
+
+  async.series(queue, callback);
+}
 
 exports.cmpBuf = cmpBuf;
 exports.testDeflate = testDeflate;
+exports.testDeflateSingle = testDeflateSingle;
+exports.loadSamples = loadSamples;
