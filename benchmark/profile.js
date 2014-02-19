028cdf58d54d25ed76f571949f8584ece8bda922
Vitaly Puzrin <vitaly@rcdesign.ru>
2014-02-19 17:43:07 +0400

    use level 6 for deflate profiling

--- benchmark/profile.js
+++ benchmark/profile.js
@@ -7,5 +7,5 @@ var data = new Uint8Array(fs.readFileSync(__dirname +'/samples/lorem_1mb.txt'));
 
 
 for (var i=0; i<50; i++) {
-  pako.deflate(data, { level: 1 });
+  pako.deflate(data, { level: 6 });
 }
