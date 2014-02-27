81331bb248201f5bd0648548a15b7d982af5367d
Vitaly Puzrin <vitaly@rcdesign.ru>
2014-02-28 01:46:32 +0400

    IE fixes

--- test/browser/test.js
+++ test/browser/test.js
@@ -6,9 +6,15 @@
 
 var size = 100*1000;
 
-var data_bin = Uint8Array ? new Uint8Array(size) : new Array(size);
+var data_bin = (typeof Uint8Array !== 'undefined') ? new Uint8Array(size) : new Array(size);
 
 for (var i=data_bin.length-1; i>=0; i--) { data_bin[i] = (Math.random(256)*256) & 0xff; }
+
+var cmp = function (a, b) {
+  if (a.length !== b.length) { return false; }
+  for (var i=0, l=a.length; i<l; i++) { if (a[i] !== b[i]) { return false; } }
+  return true;
+};
 
 
 describe('Generic tests', function () {
@@ -18,9 +24,9 @@ describe('Generic tests', function () {
   });
 
   it.skip('levels', function() {
-    assert.deepEqual(data_bin, pako.inflate(pako.deflate(data_bin)));
-    assert.deepEqual(data_bin, pako.inflate(pako.deflate(data_bin, { level: 0 })));
-    assert.deepEqual(data_bin, pako.inflate(pako.deflate(data_bin, { level: 1 })));
+    assert(cmp(data_bin, pako.inflate(pako.deflate(data_bin))));
+    assert(cmp(data_bin, pako.inflate(pako.deflate(data_bin, { level: 0 }))));
+    assert(cmp(data_bin, pako.inflate(pako.deflate(data_bin, { level: 1 }))));
   });
 });
 
