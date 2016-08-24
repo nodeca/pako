8e73b4f0695b2f42a2c07df76b9e0e3c5abb763b
Vitaly Puzrin <vitaly@rcdesign.ru>
2016-08-25 02:18:21 +0300

    Added example with sending utf-8 data

--- examples/server.js
+++ examples/server.js
@@ -5,20 +5,6 @@
 const http = require('http');
 const pako = require('../');
 
-////////////////////////////////////////////////////////////////////////////////
-// This is the main part of example
-
-function processData(bin) {
-  // Decompress binary content or POST request
-  let uncompressed = pako.inflate(new Uint8Array(bin), { to: 'string' });
-
-  // Convert utf8 -> utf16
-  let decoded = decodeURIComponent(escape(uncompressed));
-
-  console.log(decoded);
-}
-
-////////////////////////////////////////////////////////////////////////////////
 
 const server = http.createServer((req, res) => {
   var buf = [];
@@ -26,10 +12,47 @@ const server = http.createServer((req, res) => {
   req.on('data', data => buf.push(data));
 
   req.on('end', () => {
+    console.log('--- received request');
+
+    // In ideal world, we should process data as stream to minimize memory use
+    // on big data (and use node's `zlib` inflate).
+    //
+    // But that's just a quick sample to explain data reencoding steps from
+    // browser to server. Feel free to improve.
+
+    // Join all pending chunks.
     let bin = Buffer.concat(buf);
 
-    processData(bin);
+    // Test header to understand if data was sent as raw binary (modern browser)
+    // or string (utf8) format. If utf8 - reencode to binary.
+    //
+    // We could also use base64 encoding for strings on client side, but that
+    // needs more libraries for old browsers (for unsupported `window.btoa()`).
+    //
+    // If you don't need IE8/9 support - just drop this part.
+    if (req.headers['content-type'] &&
+        /text\/plain/i.test(req.headers['content-type']) &&
+        /UTF-8/i.test(req.headers['content-type'])) {
+      console.log('--- data has utf-8 encoding');
 
+      bin = Buffer.from(bin.toString(), 'binary');
+    }
+
+    // Decompress binary content
+    // Note! Can throw error on bad data
+    let uncompressed = pako.inflate(new Uint8Array(bin), { to: 'string' });
+
+    // Convert utf8 -> utf16 (native JavaScript string format)
+    let decoded = decodeURIComponent(escape(uncompressed));
+
+    // Finally, create an object
+    // Note! Can throw error on bad data
+    let obj = JSON.parse(decoded);
+
+    console.log('--- received object is: ', obj);
+
+    // Quick hack to bypass security restrictions when demo html is opened from
+    // file system. Don't do such things on production.
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.end('ok');
   });
