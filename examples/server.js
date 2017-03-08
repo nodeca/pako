61f59e7909d8c10703a42e96b333ea336a481112
Vitaly Puzrin <vitaly@rcdesign.ru>
2017-03-08 12:53:24 +0300

    Examples: send data as form

--- examples/server.js
+++ examples/server.js
@@ -2,39 +2,76 @@
 
 /*eslint-disable no-console*/
 
-const http = require('http');
-const pako = require('../');
+const http       = require('http');
+const pako       = require('../');
+const multiparty = require('multiparty');
+const Promise    = require('bluebird');
+const fs         = require('fs');
+
+
+const MULTIPART_RE    = /^multipart\/form-data(?:;|$)/i;
+const MAX_FIELDS_SIZE = 100 * 1024;       // 100kb
+const MAX_FILES_SIZE  = 10 * 1024 * 1024; // 10mb
+
+
+function error(msg) {
+  let e = new Error(msg);
+  e.statusCode = 400;
+  return e;
+}
+
 
 
 const server = http.createServer((req, res) => {
-  var buf = [];
 
-  req.on('data', data => buf.push(data));
+  console.log('--- received request');
 
-  req.on('end', () => {
-    console.log('--- received request');
+  // Quick hack to bypass security restrictions when demo html is opened from
+  // file system. Don't do such things on production.
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  Promise.coroutine(function* () {
+    //
+    // Check request size early by header and terminate immediately for big data
+    //
+    let length = parseInt((req.headers['content-length'] || '0'), 10);
+
+    if (!length || isNaN(length)) throw error('Length required');
+
+    if (!MULTIPART_RE.test(req.headers['content-type'])) {
+      throw error('Expect form data');
+    }
+
+    let err = null;
+
+    let form = new multiparty.Form({
+      maxFieldsSize: MAX_FIELDS_SIZE,
+      maxFilesSize: MAX_FILES_SIZE
+    });
+
+    let files = yield new Promise(resolve => {
+      form.parse(req, function (e, fields, files) {
+        if (e) err = e;
+        resolve(files);
+      });
+    });
+
+    if (err) {
+      err.statusCode = err.statusCode || 400;
+      throw err;
+    }
 
     // In ideal world, we should process data as stream to minimize memory use
     // on big data (and use node's `zlib` inflate).
     //
     // But that's just a quick sample to explain data reencoding steps from
     // browser to server. Feel free to improve.
+    let bin = yield Promise.fromCallback(cb => {
+      fs.readFile(files.binson[0].path, cb);
+    });
 
-    // Join all pending chunks.
-    let bin = Buffer.concat(buf);
-
-    // Test header to understand if data was sent as raw binary (modern browser)
-    // or string (utf8) format. If utf8 - reencode to binary.
-    //
-    // We could also use base64 encoding for strings on client side, but that
-    // needs more libraries for old browsers (for unsupported `window.btoa()`).
-    //
-    // If you don't need IE8/9 support - just drop this part.
-    if (/UTF-8/i.test(String(req.headers['content-type']))) {
-      console.log('--- data has utf-8 encoding');
-
-      bin = Buffer.from(bin.toString(), 'binary');
-    }
+    // Kludge - here we should cleanup all files
+    fs.unlinkSync(files.binson[0].path);
 
     // Decompress binary content
     // Note! Can throw error on bad data
@@ -48,17 +85,14 @@ const server = http.createServer((req, res) => {
     let obj = JSON.parse(decoded);
 
     console.log('--- received object is: ', obj);
-
-    // Quick hack to bypass security restrictions when demo html is opened from
-    // file system. Don't do such things on production.
-    res.setHeader('Access-Control-Allow-Origin', '*');
     res.end('ok');
+  })()
+  .catch(err => {
+    console.log(err);
+    res.statusCode = err.statusCode || 400;
+    res.end(err.message);
   });
 
-  req.on('error', () => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.end('error');
-  });
 });
 
 server.listen(8000);
