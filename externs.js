385331aef341851d7d50a39c6d94b52dd409be35
Yohan Launay <yohan.launay@gmail.com>
2017-10-24 09:09:57 +0800

    Closure Externs file
    Submitting an externs.js file for Google closure compilation. Only submitting the most commonly used inflate/deflate methods.

---
+++ externs.js
@@ -0,0 +1,18 @@
+/**
+ * @fileoverview Externs for compilation using Google closure compiler.
+ *
+ * @externs
+ */
+var pako = {};
+
+/**
+ * @param {!Uint8Array} data
+ * @return {!Uint8Array}
+ */
+pako.inflate = function(data) {};
+
+/**
+ * @param {string|!Uint8Array} data
+ * @return {!Uint8Array}
+ */
+pako.deflate = function(data) {};
